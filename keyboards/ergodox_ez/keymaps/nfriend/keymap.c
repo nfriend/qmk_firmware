7888f118c8443d01dbfff80764ddb49c19b68d9c
Nathan Friend <nathan@gitlab.com>
2019-05-05 16:35:10 -0300

    Add star emoji

--- keyboards/ergodox_ez/keymaps/nfriend/keymap.c
+++ keyboards/ergodox_ez/keymaps/nfriend/keymap.c
@@ -1280,6 +1280,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case STAR_EMOJI:
+      if (record->event.pressed) {
+        SEND_STRING(":star:");
+      }
+      return false;
+      break;
   }
   return true;
 }
