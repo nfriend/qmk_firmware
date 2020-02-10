4e0ce73c85558d78ca0ccac9165262c95330478b
Nathan Friend <nathan@gitlab.com>
2020-02-10 16:17:35 -0500

    Removing EZ configurator code

--- keyboards/ergodox_ez/keymaps/nfriend/ergodox_ez_nathan-friend_x9MWL/ergodox_ez_nathan-friend_source/rules.mk
+++
@@ -1,5 +0,0 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-DEBOUNCE_TYPE = eager_pk
-EXTRAFLAGS += -flto
-COMMAND_ENABLE = no
